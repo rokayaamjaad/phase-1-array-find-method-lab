3624120fa2820b8f9959b40668e34c98b2ddb727
rokayaamjaad <Rukayaamged733@gmail.com>
2023-01-24 23:57:30 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+function superbowlWin (record) {
+  const result = record.find( record => record.result === "W" );
+  return result ? result.year : undefined;
+}
